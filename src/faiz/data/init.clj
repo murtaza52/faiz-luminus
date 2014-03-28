2a68a5be21e18b66c93bad0f9c0b2ae55f0a8397
Murtaza Husain <murtaza52@gmail.com>
2014-03-28 21:47:13 +0530

    a

--- src/faiz/data/init.clj
+++
@@ -1,7 +0,0 @@
-;; (ns faiz.data.init
-;;   (:require [faiz.data.datomic :as dt]
-;;             [faiz.data.api :as api]
-;;             [faiz.config :refer [config]]
-;;             [faiz.utils :as utils]
-;;             [faiz.config :as config]))
-
